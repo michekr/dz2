4ea0de1fc0eed790c1b69c34bf84b69348ac6754
miche <vanya0967156586@gmail.com>
2025-05-17 19:49:19 +0300

    дз2

--- dez 2.py
+++ dez 2.py
@@ -22,3 +22,13 @@ class Employee:
 
     def get_salary_info(self):
         return f"Заробітня плата {self.name}: {self.salary} грн."
+
+
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def get_info(self):
+        return f"[{self.year}] {self.make} {self.model}"
